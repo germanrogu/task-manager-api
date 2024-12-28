ce1e2198a6a850cf759e8aa252e64f89e708b660
germanrogu <german.rodriguezgu@gmail.com>
2024-12-28 08:10:20 -0500

    Use swagger

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const { swaggerUi, swaggerSpec } = require("./docs/swagger");
 
 dotenv.config();
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Swagger docs available at http://localhost:${PORT}/docs`);
 });
